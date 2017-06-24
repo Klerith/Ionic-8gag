f62cae90cd0501b6bc9316d63a05fb9fb664cec6
Edilberto Amado P <edampe01@gmail.com>
2017-06-23 19:32:59 -0500

    Hasta la actualización de Firebase

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB7mK-Z13l6qb0CbY8u3iJot_aa-yRhUdY",
+    authDomain: "gag-bb94c.firebaseapp.com",
+    databaseURL: "https://gag-bb94c.firebaseio.com",
+    projectId: "gag-bb94c",
+    storageBucket: "gag-bb94c.appspot.com",
+    messagingSenderId: "903933209700"
+};
---
+++ src/pages/subir/subir.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ViewController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-subir',
+  templateUrl: 'subir.html',
+})
+export class SubirPage {
+
+  constructor( private viewCtrl: ViewController ) {
+  }
+
+  cerrar(){
+
+    this.viewCtrl.dismiss()
+
+  }
+
+
+}
